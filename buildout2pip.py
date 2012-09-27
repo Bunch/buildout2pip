709dcf44371c4f135cd8325d437d59f6a417f3cb
Cyril <cyrilrbt@gmail.com>
2012-09-27 11:31:37 -0400

    indentation / pep8 fix

--- buildout2pip.py
+++ buildout2pip.py
@@ -6,63 +6,64 @@
 import sys
 import ConfigParser
 
+
 def main(buildoutcfg):
-	config = ConfigParser.ConfigParser()
-	config.read(buildoutcfg)
+    config = ConfigParser.ConfigParser()
+    config.read(buildoutcfg)
 
-	# First get list of eggs
-	temp_eggs = config.get('eggs', 'eggs').split('\n')
-	eggs = {}
-	# clean up comments
-	for egg in temp_eggs:
-		eggs[egg.split()[0]] = ''
+    # First get list of eggs
+    temp_eggs = config.get('eggs', 'eggs').split('\n')
+    eggs = {}
+    # clean up comments
+    for egg in temp_eggs:
+        eggs[egg.split()[0]] = ''
 
-	# Next need to match with versions
-	for egg, version in config.items('versions'):
-		if egg in eggs:
-			eggs[egg] = version
+    # Next need to match with versions
+    for egg, version in config.items('versions'):
+        if egg in eggs:
+            eggs[egg] = version
 
-	# Next we need to incorporate sources
-	for egg, source in config.items('sources'):
-		if egg in eggs:
-			options = None
-			(rcs, url) = source.split(None, 1)
-			# account for extra options
-			try:
-				(url, options) = url.split()
-			except ValueError:
-				pass
+    # Next we need to incorporate sources
+    for egg, source in config.items('sources'):
+        if egg in eggs:
+            options = None
+            (rcs, url) = source.split(None, 1)
+            # account for extra options
+            try:
+                (url, options) = url.split()
+            except ValueError:
+                pass
 
-			# Format the URL properly
-			if url.startswith('git@'):
-				# git@github.com:MyProject/project.git
-				# git+ssh://git@myproject.org/MyProject/#egg=MyProject
-				(userhost, uri) = url.split(':')
-				url = "git+ssh://%s/%s" % (userhost, uri)
-			elif url.startswith('http'):
-				url = "git+%s" % url
+            # Format the URL properly
+            if url.startswith('git@'):
+                # git@github.com:MyProject/project.git
+                # git+ssh://git@myproject.org/MyProject/#egg=MyProject
+                (userhost, uri) = url.split(':')
+                url = "git+ssh://%s/%s" % (userhost, uri)
+            elif url.startswith('http'):
+                url = "git+%s" % url
 
-			if options:
-				(key, val) = options.split('=')
-				if key in ['rev', 'branch']:
-					eggs[egg] = "-e %s@%s#egg=%s" % (url, val, egg)
-				else:
-					print "Unknown option found: %s" % key
-			else:
-				eggs[egg] = "-e %s#egg=%s" % (url, egg)
-	
-	# Putting it all together
-	for egg in eggs:
-		if eggs[egg].startswith('-e'):
-			print eggs[egg]
-		elif eggs[egg] == '':
-			print egg
-		else:
-			print "%s==%s" % (egg, eggs[egg])
+            if options:
+                (key, val) = options.split('=')
+                if key in ['rev', 'branch']:
+                    eggs[egg] = "-e %s@%s#egg=%s" % (url, val, egg)
+                else:
+                    print "Unknown option found: %s" % key
+            else:
+                eggs[egg] = "-e %s#egg=%s" % (url, egg)
+
+    # Putting it all together
+    for egg in eggs:
+        if eggs[egg].startswith('-e'):
+            print eggs[egg]
+        elif eggs[egg] == '':
+            print egg
+        else:
+            print "%s==%s" % (egg, eggs[egg])
 
 if __name__ == "__main__":
-	try:
-		buildoutcfg = sys.argv[1]
-	except:
-		buildoutcfg = "buildout.cfg"
-	main(buildoutcfg)
+    try:
+        buildoutcfg = sys.argv[1]
+    except:
+        buildoutcfg = "buildout.cfg"
+    main(buildoutcfg)
